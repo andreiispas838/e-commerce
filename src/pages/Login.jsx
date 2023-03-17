1964f590ce15ac6796a901a1df778c4d0031ed49
Andrei Ispas <andreiispas838@yahoo.com>
2023-03-17 06:29:06 +0200

    register & log in

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,72 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url('https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 30%;
+  padding: 1.25rem;
+  background-color: white;
+`;
+
+const Title = styled.h1`
+  font-size: 1.5rem;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 0.6rem 0;
+  padding: 0.6rem;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 1.25rem 0.6rem;
+  color: white;
+  background-color: teal;
+  cursor: pointer;
+  margin-bottom: 0.6rem;
+`;
+
+const Link = styled.a`
+  margin: 0.3rem 0;
+  font-size: 0.75rem;
+  text-decoration: underline;
+  cursor: pointer;
+`;
 
 const Login = () => {
-  return <Container>Login</Container>;
+  return (
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input placeholder='username' />
+          <Input placeholder='password' />
+
+          <Button>LOGIN</Button>
+          <Link>I FORGOT MY PASSWORD</Link>
+          <Link>CREATE A NEW ACCOUNT</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,73 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url('https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 40%;
+  padding: 1.25rem;
+  background-color: white;
+`;
+
+const Title = styled.h1`
+  font-size: 1.5rem;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 1.25rem 0.6rem 0 0;
+  padding: 0.6rem;
+`;
+
+const Agreement = styled.span`
+  font-size: 0.75rem;
+  margin: 1.25rem 0;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 1.25rem 0.6rem;
+  color: white;
+  background-color: teal;
+  cursor: pointer;
+`;
 
 const Register = () => {
-  return <Container>Register</Container>;
+  return (
+    <Container>
+      <Wrapper>
+        <Title>CREATE AN ACCOUNT</Title>
+        <Form>
+          <Input placeholder='first name' />
+          <Input placeholder='last name' />
+          <Input placeholder='username' />
+          <Input placeholder='email' />
+          <Input placeholder='password' />
+          <Input placeholder='confirm password' />
+          <Agreement>
+            By creating an account, I consent to the processing of my personal data in accordance with the <b>PRIVACY POLICY</b>
+          </Agreement>
+          <Button>CREATE</Button>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Register;
